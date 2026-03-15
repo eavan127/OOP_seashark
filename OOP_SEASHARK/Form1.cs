5da5e335752529753566f2f5e7a71390d3d340cb
Jessy123123 <xinyuanpeng1@gmail.com>
2026-03-15 21:22:47 +0800

    Update

--- OOP_SEASHARK/Form1.cs
+++ OOP_SEASHARK/Form1.cs
@@ -12,7 +12,9 @@ namespace OOP_SEASHARK
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
         }
 
         private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
