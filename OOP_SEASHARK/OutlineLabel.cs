37c1683745875f295f8297173bba9a7ac04d5187
Jessy123123 <xinyuanpeng1@gmail.com>
2026-03-29 15:10:52 +0800

    remove duplicate file

--- OOP_SEASHARK/OutlineLabel.cs
+++ OOP_SEASHARK/OutlineLabel.cs
@@ -2,6 +2,8 @@
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
+namespace MyApp.Controls;
+
 public class OutlineLabel : Label
 {
     public Color OutlineColor { get; set; } = Color.White;
@@ -9,7 +11,7 @@ public class OutlineLabel : Label
 
     public OutlineLabel()
     {
-        this.AutoSize = false;
+        this.AutoSize = false;  
         this.BackColor = Color.Transparent;
     }
 
@@ -20,39 +22,37 @@ public class OutlineLabel : Label
 
         using (GraphicsPath path = new GraphicsPath())
         {
-            StringFormat sf = new StringFormat();
-
-            if (this.TextAlign == ContentAlignment.MiddleCenter)
+            using (StringFormat sf = new StringFormat())
             {
-                sf.Alignment = StringAlignment.Center;
-                sf.LineAlignment = StringAlignment.Center;
+                if (this.TextAlign == ContentAlignment.MiddleCenter)
+                {
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
+                }
+                else if (this.TextAlign == ContentAlignment.MiddleLeft)
+                {
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Center;
+                }
+                else if (this.TextAlign == ContentAlignment.MiddleRight)
+                {
+                    sf.Alignment = StringAlignment.Far;
+                    sf.LineAlignment = StringAlignment.Center;
+                }
+
+                float emSize = e.Graphics.DpiY * this.Font.Size / 72f;
+
+                path.AddString(
+                    this.Text ?? string.Empty,
+                    this.Font.FontFamily,
+                    (int)this.Font.Style,
+                    emSize,
+                    this.ClientRectangle,
+                    sf
+                );
             }
-            else if (this.TextAlign == ContentAlignment.MiddleLeft)
-            {
-                sf.Alignment = StringAlignment.Near;
-                sf.LineAlignment = StringAlignment.Center;
-            }
-            else if (this.TextAlign == ContentAlignment.MiddleRight)
-            {
-                sf.Alignment = StringAlignment.Far;
-                sf.LineAlignment = StringAlignment.Center;
-            }
 
-            float emSize = e.Graphics.DpiY * this.Font.Size / 72;
-
-            path.AddString(
-                this.Text,
-                this.Font.FontFamily,
-                (int)this.Font.Style,
-                emSize,
-                this.ClientRectangle,
-                sf
-            );
-
-            using (Pen outlinePen = new Pen(OutlineColor, OutlineWidth)
-            {
-                LineJoin = LineJoin.Round
-            })
+            using (Pen outlinePen = new Pen(this.OutlineColor, this.OutlineWidth) { LineJoin = LineJoin.Round })
             {
                 e.Graphics.DrawPath(outlinePen, path);
             }
