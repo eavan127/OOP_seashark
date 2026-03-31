41e47fc4d5d80309f5614c02dd5246d0ccdae3d0
chuaxinying111 <chuaying46@gmail.com>
2026-04-01 01:12:02 +0800

    Comment

--- OOP_SEASHARK/frmHint.cs
+++ OOP_SEASHARK/frmHint.cs
@@ -21,27 +21,27 @@ namespace OOP_GroupProject
             this.MinimizeBox = false;
             this.StartPosition = FormStartPosition.CenterParent;
             this.Text = "Helpful Tips";
-            
+
             label2.Text = hintText;
-            
+
             // Fix layout after components are initialized
             SetupLayout();
         }
 
         private void SetupLayout()
         {
-            // ensure the main decorative background covers enough space
+            // Ensure the main decorative background covers enough space
             pictureBox3.Size = new Size(this.ClientSize.Width - 80, this.ClientSize.Height - 160);
             pictureBox3.Location = new Point(40, 80);
 
-            // ensure the hint text fits perfectly within the blue box and wraps correctly
+            // Ensure the hint text fits perfectly within the blue box and wraps correctly
             label2.AutoSize = false;
             label2.TextAlign = ContentAlignment.MiddleCenter;
             label2.Size = new Size(pictureBox3.Width - 40, pictureBox3.Height - 40);
             label2.Location = new Point(pictureBox3.Left + 20, pictureBox3.Top + 20);
             label2.BringToFront();
 
-            // center the "Got It" button
+            // Center the "Got It" button
             button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, this.ClientSize.Height - 70);
             button1.BringToFront();
         }
@@ -49,6 +49,11 @@ namespace OOP_GroupProject
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
